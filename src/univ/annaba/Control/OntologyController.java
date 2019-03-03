02a03f396f59e4ba6c66007ab0c96659aa4bc6f1
Moussa Saker <sak_moussa@yahoo.fr>
2019-03-03 13:03:11 +0100

    creating Ontology individuals

--- src/univ/annaba/Control/OntologyController.java
+++ src/univ/annaba/Control/OntologyController.java
@@ -5,17 +5,19 @@ import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
+import java.util.Hashtable;
 import java.util.Iterator;
 
 import org.apache.jena.ontology.OntClass;
 import org.apache.jena.ontology.OntModel;
 import org.apache.jena.ontology.OntModelSpec;
+import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.util.FileManager;
 
 public class OntologyController {
 	protected OntModel ontology;
-	private MyVisitor visitor;
+	protected MyVisitor visitor;
 	protected String ontologyPath = "/home/moise/Documents/example/OntoCode.owl";
 	protected String ontologyURI = "http://www.semanticweb.org/toshiba/ontologies/2017/4/untitled-ontology-77#";
 	
@@ -26,8 +28,7 @@ public class OntologyController {
 	public void writeOntology( String ontologyOutPutPath) {
 		OutputStream out;
 		OntModel ontology = ModelFactory.createOntologyModel();
-		
-		this.buildOntology(ontology);
+		ontology = this.buildOntology(ontology);
 		try {
 			out = new FileOutputStream(ontologyOutPutPath);
 			ontology.write(out, "RDF/XML");
@@ -39,40 +40,81 @@ public class OntologyController {
 	}
 	
 	public OntModel readOntology(String ontologyInputPath) {
-		
 		ontology = ModelFactory.createOntologyModel();
-		// use the FileManager to find the input file
 		InputStream in = FileManager.get().open(ontologyInputPath);
 		if (in == null) {
-			throw new IllegalArgumentException("File: " + ontologyPath + " not found");
+			throw new IllegalArgumentException("File: " + ontologyInputPath + " not found");
 		}
-		ontology.read(in, "", "RDF/XML");
+		ontology.read(in,"RDF/XML");
 		return ontology;
 	}
 	
-	public OntModel addOntologyElement(ArrayList<String> concepts, String ontologyPath) {
+	public OntModel addOntologyElement(String ontologyPath, ArrayList<String> concepts) {
 		OntModel ontology = this.readOntology(ontologyPath);
-		System.out.println(ontology);
-		System.out.println(concepts);
-		Iterator<String> i = concepts.iterator();
+		Hashtable<String, ArrayList<String>> hash = visitor.getConceptsReport();
+		
+		if (hash.containsKey("Methods")) {
+		ArrayList<String> methods = hash.get("Methods");
+		for (int i = 0; i < methods.size(); i++) {
+				OntClass ontClass =  ontology.getOntClass(ontology.getNsPrefixURI("")+"Method");
+				ontClass.createIndividual(ontologyURI + methods.get(i));
+			}
+		}
+		if (hash.containsKey("Classes")) {
+			ArrayList<String> classes = hash.get("Classes");
+			for (int i = 0; i < classes.size(); i++) {
+					OntClass ontClass =  ontology.getOntClass(ontology.getNsPrefixURI("")+"Classs");
+					ontClass.createIndividual(ontologyURI + classes.get(i));
+				}
+		}
+		if (hash.containsKey("Packages")) {
+			ArrayList<String> packages = hash.get("Packages");
+			for (int i = 0; i < packages.size(); i++) {
+					OntClass ontClass =  ontology.getOntClass(ontology.getNsPrefixURI("")+"Package");
+					ontClass.createIndividual(ontologyURI + packages.get(i));
+				}
+		}
+		if (hash.containsKey("Variables")) {
+			ArrayList<String> variables = hash.get("Variables");
+			for (int i = 0; i < variables.size(); i++) {
+					OntClass ontClass =  ontology.getOntClass(ontology.getNsPrefixURI("")+"Variable");
+					ontClass.createIndividual(ontologyURI + variables.get(i));
+				}
+		}
+			return ontology;
+	}
+	
+	/**
+	public OntModel addOntologyMethods(String ontologyPath, ArrayList<String> methods) {
+		OntModel ontology = this.readOntology(ontologyPath);
+		Iterator<String> i = methods.iterator();
 		while (i.hasNext()) {
-			String d = (String) i.next();
-			System.out.println("the iteraotor result: "+d);
-			OntClass ontClass =  ontology.getOntClass(ontologyURI);
-			
-			System.out.println("this is the ontClass: "+ontology.getOntClass(ontologyURI));
-			
-			ontology.createIndividual(ontologyURI + d, ontClass);
+			String method = (String) i.next();
+			OntClass ontClass =  ontology.getOntClass(ontology.getNsPrefixURI("")+"Method");
+			ontClass.createIndividual(ontologyURI+method);
 		}
 		return ontology;
 	}
+	
+	public OntModel addOntologyClasses(String ontologyPath, ArrayList<String> classes) {
+		OntModel ontology = this.readOntology(ontologyPath);
+		Iterator<String> i = classes.iterator();
+		while (i.hasNext()) {
+			String class1 = (String) i.next();
+			OntClass ontClass =  ontology.getOntClass(ontology.getNsPrefixURI("")+"Class");
+			ontClass.createIndividual(ontologyURI+class1);
+		}
+		return ontology;
+	}
+	*/
 	
 	public OntModel buildOntology(OntModel ontology){
-		ontologyPath= "/home/moise/Documents/example/OntoCode.owl";
-		ontology = this.addOntologyElement(visitor.getMethods(),ontologyPath);
-		//ontology = this.addOntologyElement(visitor.getClasses(), ontologyPath);
-		//ontology = this.addOntologyElement(visitor.getPackages(), ontologyPath);
-		//ontology = this.addOntologyElement(visitor.getVariables(), ontologyPath);
+		
+		ontology = this.addOntologyElement(ontologyPath, visitor.getMethods());
+		//ontology = this.addOntologyElement(ontologyPath, visitor.getClasses(),"Classs");
+		//ontology = this.addOntologyElement(ontologyPath, visitor.getPackages(),"Package");
+		//ontology = this.addOntologyElement(ontologyPath, visitor.getVariables(),"Variable");
+
 		return ontology;
 	}
 	
@@ -85,10 +127,11 @@ public class OntologyController {
 	}
 	
 	public static void main(String[] args) {
+		String ontologyURI = "http://www.semanticweb.org/toshiba/ontologies/2017/4/untitled-ontology-77#";
 		MyVisitor myVisitor = new MyVisitor("/home/moise/Documents/example/HelloWorld.java");
 		OntologyController controller = new OntologyController(myVisitor);
-		OntModel ontModel = controller.readOntology("/home/moise/Documents/example/OntoCode.owl");
+		OntModel model = controller.readOntology("/home/moise/Documents/example/OntoCode.owl");
 		controller.writeOntology("/home/moise/Documents/example/OntoCode.owl");
-		
+	
 	}
 }
